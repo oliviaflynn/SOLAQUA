6badd3f6e7877795c19e9f9576be4c56ef53830a
oflynn <livjflynn@gmail.com>
2018-07-16 12:54:54 +0100

    second commit

--- js/hello.js
+++ js/hello.js
@@ -1,5 +1,9 @@
-var $ = require('jquery');
-
-$(function() {
-    alert('it works');
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
