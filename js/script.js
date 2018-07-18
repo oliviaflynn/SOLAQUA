2d15b79eb8458199e7a8b2f46457114501cd3708
oflynn <livjflynn@gmail.com>
2018-07-18 14:50:36 +0100

    working commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*
+
+NAV
+
+*/
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -6,4 +12,29 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
+} 
+
+
+/*
+
+Back To Top Button
+
+*/
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topButton").style.display = "block";
+    } else {
+        document.getElementById("topButton").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
